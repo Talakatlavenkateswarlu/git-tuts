c2790e6f5dd7fc05d8e038dcc1e4031319d20ca1
T venkateswarlu <talakatlavenkateswarlu@gmail.com>
2023-08-04 11:30:05 +0530

    added arrow functions

---
+++ practice/Javascript/arrowfunctions.js
@@ -0,0 +1,30 @@
+function add(n1, n2)
+{
+   console.log(n1+n2)
+}
+
+add(3,15);
+
+add =(n1, n2)=>
+{
+   console.log(n1+n2)
+}
+
+add(3,15);
+
+
+
+function check()
+{
+   alert("normal function")
+}
+
+check();
+
+
+checking =()=>
+{
+    alert("arrow function")
+}
+checking();
+
