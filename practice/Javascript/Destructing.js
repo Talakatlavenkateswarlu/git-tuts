63d8ba2a9ebe6cc6458523725b8079c9ebba4095
T venkateswarlu <talakatlavenkateswarlu@gmail.com>
2023-08-04 22:45:05 +0530

    added destructering

---
+++ practice/Javascript/Destructing.js
@@ -0,0 +1,17 @@
+
+console.log("hi am venky")
+
+let arr = [10,16,13,18]
+
+let [ele1,ele2,ele3,ele4,ele5] = arr
+
+console.log(ele1,ele2,ele3,ele4,ele5) 
+
+let course = {
+    'topic1':'html',
+    'topic2':'css',
+    'topic3':'js'
+}
+ let {topic1,topic2,topic3} = course
+             
+console.log(topic1,topic2,topic3);
